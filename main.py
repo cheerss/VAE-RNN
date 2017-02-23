8456e28fe4fb0ebfa9f03716c51bc1acc901490f
cheerss <zjdxwwx@163.com>
2017-02-23 10:38:44 +0800

    initial

---
+++ main.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+import vgg
+import numpy as np
+
+
+width = 256
+height = 256
+
+def main():
+
+# train the network
+def train():
+	with tf.Session() as sess:
+		image = tf.placeholder(tf.float32, shape = [width, height])
+		tf.
+
+
+# generate a new picture
+def generate():
+
+if __name__ == '__main__':
+	main()
